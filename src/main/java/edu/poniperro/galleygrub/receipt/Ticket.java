f9a545f75b69ae47241f0d69f088d7eccc84d5ae
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-02-17 19:08:26 +0100

    :recycle: Interfaz Ticket métodos en TODO

--- src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
+++ src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
@@ -1,11 +1,12 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extra;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public interface Ticket {
     Comanda getOrder();
-    //void setChain(Extra extra); // TODO Extra
-    //Extra getChain(); // TODO Extra
+    void setChain(Extra extra);
+    Extra getChain();
     double total();
     void sumExtrasCharge();
     void print();
