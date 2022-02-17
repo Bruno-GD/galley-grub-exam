338da9b6cf42fadfe627bf1db859ca8c4bdc13da
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-02-17 20:17:04 +0100

    :bug: "Regular" sumar extras

--- src/main/java/edu/poniperro/galleygrub/extras/Regular.java
+++ src/main/java/edu/poniperro/galleygrub/extras/Regular.java
@@ -1,8 +1,16 @@
 package edu.poniperro.galleygrub.extras;
 
+import edu.poniperro.galleygrub.items.Item;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public class Regular extends Extra {
     @Override
-    public void sumExtras(Comanda order) {}
+    public void sumExtras(Comanda order) {
+        double regularSum = order.itemList().stream()
+                        .filter(Item::isRegular)
+                        .mapToDouble(Item::price)
+                        .sum();
+        order.updateTotal(regularSum);
+        nextExtra.ifPresent(extra -> extra.sumExtras(order));
+    }
 }
