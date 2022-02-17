42c048dc5b4b27a5dc733932d85661eba06068e3
Bruno-GD <91556467+Bruno-GD@users.noreply.github.com>
2022-02-17 17:54:34 +0100

    :sparkles: Interfaz "Comanda"

---
+++ src/main/java/edu/poniperro/galleygrub/order/Comanda.java
@@ -0,0 +1,15 @@
+package edu.poniperro.galleygrub.order;
+
+import edu.poniperro.galleygrub.items.Item;
+
+import java.util.List;
+
+public interface Comanda {
+    void addItem(String name, Double price);
+    void addItem(String name, Double price, String extra);
+    int size();
+    List<Item> itemList();
+    double getTotal();
+    void updateTotal();
+    void display();
+}
